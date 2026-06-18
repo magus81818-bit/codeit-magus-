8a4a94fc162e4885c00e73a3a015130e3ab2b8c9
magus81818-bit <magus81818@gmail.com>
2026-06-18 18:17:30 +0900

    브라우저 화면 문구 정리

--- src/app-oop/ui/browser/memo-screen.ts
+++ src/app-oop/ui/browser/memo-screen.ts
@@ -2,7 +2,7 @@ export class MemoScreen {
   showGrettingMessage(username: string): void {
     const element = document.getElementById("greeting-message");
     if (element !== null) {
-      element.innerText = `${username}의 메모장`;
+      element.innerText = `${username}님의 메모장`;
     }
   }
 }
